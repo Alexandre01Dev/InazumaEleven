c40fa25e3477c9c868a402fc86bd65b0ef0e03b2
Nan0uche <78554163+Nan0uche@users.noreply.github.com>
2021-05-23 16:44:52 +0200

    cc

--- src/main/java/be/alexandre01/inazuma_eleven/roles/alius/David.java
+++ src/main/java/be/alexandre01/inazuma_eleven/roles/alius/David.java
@@ -57,130 +57,15 @@ public class David extends Role implements Listener {
         onLoad(new load() {
             @Override
             public void a(Player player) {
-                Bukkit.getScheduler().runTaskLater(inazumaUHC, new Runnable() {
+                onLoad(new load() {
                     @Override
-                    public void run() {
-                        InazumaUHC.get.dm.addEffectPourcentage(player, DamageManager.EffectType.INCREASE_DAMAGE,2,117);
-
-                        if(bukkitTask != null){
-                            bukkitTask.cancel();
-                        }
-                        if(accepted){
-                            if (player.getMaxHealth()>8){
-                                player.sendMessage(Preset.instance.p.prefixName()+" Vous avez perdu §c§l0.5 §4❤§7 permanent suite à avoir accepté le §c§lManchot §c§lEmpereur §4§lN°1§7.");
-                                PatchedEntity.setMaxHealthInSilent(player, player.getMaxHealth()-1);
-                            }
-                            return;
-                        }
-
-                        sendRequest();
-
-                        bukkitTask = Bukkit.getScheduler().runTaskLaterAsynchronously(inazumaUHC, () -> {
-                            hasChoose = true;
-                        },20*60*5);
-                        hasChoose = false;
+                    public void a(Player player) {
+                        inazumaUHC.dm.addEffectPourcentage(player, DamageManager.EffectType.RESISTANCE,1,120);
                     }
-                },20*3);
+
+                });
             }
         });
-        addCommand("manchot", new command() {
-            @Override
-            public void a(String[] args, Player player) {
-                if(hasChoose){
-                    player.sendMessage(Preset.instance.p.prefixName()+" Vous ne pouvez pas utiliser cette commande pour le moment");
-                    return;
-                }
-                if(args.length == 0){
-                    player.sendMessage(Preset.instance.p.prefixName()+" Veuillez mettre /manchot §aaccept §7ou §a/manchot §crefuse");
-                    return;
-                }
 
-                if(args[0].equalsIgnoreCase("accept")){
-                    hasChoose = true;
-                    accepted = true;
-                    bukkitTask.cancel();
-                    RoleItem roleItem = new RoleItem();
-                    roleItem.deployVerificationsOnRightClick(roleItem.generateVerification(new Tuple<>(RoleItem.VerificationType.EPISODES,1)));
-
-
-                    ItemBuilder it = new ItemBuilder(Material.NETHER_STAR).setName("§c§lManchot §c§lEmpereur §4§lN°1");
-
-                    roleItem.setItemstack(it.toItemStack());
-
-                    roleItem.setRightClick(new RoleItem.RightClick() {
-                        @Override
-                        public void execute(Player player) {
-
-                            player.sendMessage(Preset.instance.p.prefixName()+" Vous venez d'utiliser §c§lManchot §c§lEmpereur §4§lN°1");
-                            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 2*20*60, 0,false,false), true);
-                            player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 2*20*60, 1,false,false), true);
-                        }
-                    });
-
-                    addRoleItem(roleItem);
-
-                    giveItem(player);
-
-                    PatchedEntity.setMaxHealthInSilent(player, player.getMaxHealth()-4);
-                    player.sendMessage(Preset.instance.p.prefixName()+" Vous venez de recevoir le §c§lManchot §c§lEmpereur §4§lN°1§7");
-                    return;
-                }
-                if (args[0].equalsIgnoreCase("refuse")) {
-                    hasChoose = true;
-                    bukkitTask.cancel();
-                    return;
-                }
-                player.sendMessage(Preset.instance.p.prefixName()+" Veuillez mettre /manchot §aaccept §7ou §a/manchot §crefuse");
-            }
-        });
-    }
-
-
-
-
-    private void sendRequest(){
-
-        BaseComponent b = new TextComponent(Preset.instance.p.prefixName()+" Voulez-vous recevoir votre ");
-
-        BaseComponent manchot = new TextComponent("§7[§c§lManchot §c§lEmpereur §4§lN°1§7] ? §7*§8Curseur§7*");
-        BaseComponent manchotDesc = new TextComponent();
-        manchotDesc.addExtra("§e- §9Utilisation par §eEpisode\n");
-        manchotDesc.addExtra("§e- §9Perdre immédiatement §c§l3 §4❤§7 permanent\n");
-        manchotDesc.addExtra("§e- §9Perdre §c§l0.5 §4❤§7 permanent tous les §eEpisode");
-        manchot.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,manchotDesc.getExtra().toArray(new BaseComponent[0])));
-        b.addExtra(manchot);
-
-        BaseComponent yes = new TextComponent("§a[Accepter]");
-        yes.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/manchot accept"));
-        b.addExtra(yes);
-        b.addExtra(" §7ou ");
-        BaseComponent no = new TextComponent("§c[Refuser]");
-        no.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/manchot refuse"));
-
-        b.addExtra(no);
-
-        for(Player player : getPlayers()){
-            player.spigot().sendMessage(b);
-        }
-    }
-    @EventHandler
-    public void onEpisode(EpisodeChangeEvent event){
-        if(bukkitTask != null){
-            bukkitTask.cancel();
-        }
-        if(accepted){
-            getPlayers().forEach(player -> {
-                    player.sendMessage(Preset.instance.p.prefixName()+" Vous avez perdu §c§l0.5 §4❤§7 permanent suite à avoir accepté le §c§lManchot §c§lEmpereur §4§lN°1§7.");
-                    PatchedEntity.setMaxHealthInSilent(player, player.getMaxHealth()-1);
-            });
-            return;
-        }
-
-        sendRequest();
-
-        bukkitTask = Bukkit.getScheduler().runTaskLaterAsynchronously(inazumaUHC, () -> {
-            hasChoose = true;
-            },20*60*5);
-        hasChoose = false;
     }
 }
