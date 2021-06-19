93ce519afaba8b796892923f4e076b1325e766d3
Nan0uche <78554163+Nan0uche@users.noreply.github.com>
2021-06-19 14:06:41 +0200

    test new scoreboard

--- src/main/java/be/alexandre01/inazuma_eleven/scoreboards/GameScoreboard.java
+++ src/main/java/be/alexandre01/inazuma_eleven/scoreboards/GameScoreboard.java
@@ -39,27 +39,22 @@ public class GameScoreboard {
                 public void lines(String ip, ObjectiveSign objectiveSign) {
                     objectiveSign.setDisplayName("§8»§3§lInazuma§8§l-§3§lEleven§8«");
 
-                    objectiveSign.setLine(4,    "§8§m*----§7§m--INFOS§7§m--§8§m----*");
-                    objectiveSign.setLine(5, "§7Joueurs §l» §e" + InazumaUHC.get.getRemainingPlayers().size());
-                    objectiveSign.setLine(6,"§7Episode §l» §e"+ Episode.getEpisode());
-                    objectiveSign.setLine(7, "§7Temps §l» §e" + inazuma.totalTimeValue);
-                    objectiveSign.setLine(8,    "§r§8§m*----§7§m--TIMERS§7§m--§8§m----*");
-                    objectiveSign.setLine(9, "§7"+ inazuma.pvpText+" §l» §e" + inazuma.pvpValue);
+                    objectiveSign.setLine(4,    "§8§m*----§7§m--§f§lINFOS§7§m--§8§m----*");
+                    objectiveSign.setLine(5,"§7Episode §l» §e"+ Episode.getEpisode());
+                    objectiveSign.setLine(6, "§7Joueurs §l» §e" + InazumaUHC.get.getRemainingPlayers().size());
+                    objectiveSign.setLine(7,    "§r§8§m*----§7§m--§f§lTIMERS§7§m--§8§m----*");
+                    objectiveSign.setLine(8, "§7Temps §f§l» §e" + inazuma.totalTimeValue);
+                    objectiveSign.setLine(9, "§7PvP §f§l» §e" + inazuma.pvpValue);
                     if(inazuma.hasNether()){
-                        objectiveSign.setLine(10,"§7"+inazuma.netherText+" §l» §e" + inazuma.netherValue);
+                        objectiveSign.setLine(10,"§7Meetup §f§l» §e" + inazuma.netherValue);
                     }
                     objectiveSign.setLine(11,"§7"+inazuma.bordureText+" §l» §e" + inazuma.bordureValue);
-                    objectiveSign.setLine(17,    "§8§m*----§7§m--BORDURES§7§m--§8§m----*");
+                    objectiveSign.setLine(17,    "§8§m*----§7§m--§f§lBORDURES§7§m--§8§m----*");
                     World world = player.getWorld();
                     int borderSize = (int) world.getWorldBorder().getSize() /2;
                     objectiveSign.setLine(18,"§7Bordure §l» §e-"+ borderSize+"§7/§e"+borderSize);
                     objectiveSign.setLine(19,"§7Centre §l» §e "+ inazuma.getArrows().get(player.getUniqueId()));
-                    objectiveSign.setLine(21, "§l§8»§8§m------------§l§8« ");
-
-                    objectiveSign.setLine(21, "§7Scénario(s) §l» "+scenario);
-
-
-                    objectiveSign.setLine(21, "§l§8»§8§m------------§l§8« ");
+                    objectiveSign.setLine(17,    "§8§m*----§7§m--§7§m--§8§m----*");
                     objectiveSign.setLine(22, ip);
 
                     objectiveSign.updateLines();
