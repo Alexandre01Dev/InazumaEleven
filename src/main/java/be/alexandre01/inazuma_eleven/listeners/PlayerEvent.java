635c4ae2a679e2186142354b85f51b726bc62f70
Nan0uche <78554163+Nan0uche@users.noreply.github.com>
2021-08-17 01:57:49 +0200

    auto revive

--- src/main/java/be/alexandre01/inazuma_eleven/listeners/PlayerEvent.java
+++ src/main/java/be/alexandre01/inazuma_eleven/listeners/PlayerEvent.java
@@ -4,6 +4,7 @@ import be.alexandre01.inazuma.uhc.InazumaUHC;
 import be.alexandre01.inazuma.uhc.custom_events.player.PlayerInstantDeathEvent;
 import be.alexandre01.inazuma.uhc.custom_events.roles.RoleItemTargetEvent;
 import be.alexandre01.inazuma.uhc.custom_events.roles.RoleItemUseEvent;
+import be.alexandre01.inazuma.uhc.managers.RejoinManager;
 import be.alexandre01.inazuma.uhc.presets.Preset;
 import be.alexandre01.inazuma.uhc.roles.Role;
 import be.alexandre01.inazuma.uhc.state.GameState;
@@ -11,6 +12,7 @@ import be.alexandre01.inazuma.uhc.state.State;
 import be.alexandre01.inazuma.uhc.utils.TitleUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,6 +20,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerEvent implements Listener {
     private GameState gameState;
@@ -64,6 +67,24 @@ public class PlayerEvent implements Listener {
             Bukkit.broadcastMessage(Preset.instance.p.prefixName() + " §c§l" + player.getName() + "§7 vient de mourir (PVE).");
             InazumaUHC.get.getRejoinManager().onKilled(player);
             event.getDrops().clear();
+
+            new BukkitRunnable(){
+                @Override
+                public void run(){
+
+                    RejoinManager r = InazumaUHC.get.getRejoinManager();
+                    r.revivePlayer(player);
+                    player.sendMessage(Preset.instance.p.prefixName()+" §aVous venez d'être ressuscité.");
+                    InazumaUHC.get.invincibilityDamager.addPlayer(player, 1000*7);
+                    player.sendMessage(Preset.instance.p.prefixName()+" §eVous avez 7 secondes d'invincibilité.");
+                    r.teleportRandom(player);
+                    player.setExp(player.getExp()/2);
+                    player.setLevel(player.getLevel()/2);
+
+                }
+
+            }.runTaskLater(InazumaUHC.get, 10);
+
         }
     }
 
