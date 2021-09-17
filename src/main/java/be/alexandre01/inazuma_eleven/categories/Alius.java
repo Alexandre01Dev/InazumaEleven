b62d3a736e39af2683a173bb44b4dea5ea9e67d1
Nan0uche <78554163+Nan0uche@users.noreply.github.com>
2021-09-17 18:56:10 +0200

    death megssage

--- src/main/java/be/alexandre01/inazuma_eleven/categories/Alius.java
+++ src/main/java/be/alexandre01/inazuma_eleven/categories/Alius.java
@@ -5,6 +5,6 @@ import be.alexandre01.inazuma.uhc.roles.RoleCategory;
 public class Alius extends RoleCategory {
     public Alius(String category, String prefixColor, String winMessage) {
         super(category, prefixColor, winMessage);
-        setDeathMessage(new String[]{"§5L'§lAcadémie §lAlius§7 vient de perdre un joueur,","§e%player% §7était §f: %role%"});
+        setDeathMessage(new String[]{"§5L'§lAcadémie §lAlius§7 vient de perdre un joueur,","§5%player% §7était §f: %role%"});
     }
 }
